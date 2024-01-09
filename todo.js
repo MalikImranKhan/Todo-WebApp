048d632d7570b91a774646eecfe26de74f8edb6b
Imran Khan <ik9907074@gmail.com>
2024-01-09 15:50:03 +0500

    New repo

---
+++ todo.js
@@ -0,0 +1,33 @@
+let task = document.querySelector("input");
+let btn = document.querySelector("button");
+let list = document.querySelector("ul");
+
+
+btn.addEventListener("click", function () {
+    let item = document.createElement("li");
+    item.innerText = task.value;
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    list.appendChild(item);
+    item.appendChild(delBtn);   
+    task.value = "";
+});
+
+list.addEventListener("click", function(event) {
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+})
+
+
+
+
+
+// let delBtns = document.querySelectorAll(".delete");
+// for (delBtn of delBtns){
+//     delBtn.addEventListener("click", function() {
+//         let par = this.parentElement;
+//         par.remove();
+//     })
+// }
